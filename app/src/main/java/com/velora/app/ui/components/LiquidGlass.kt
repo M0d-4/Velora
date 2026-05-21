86ba47f66793916a8fe3bea843ce1be07e88de68
Mod4 <omarelfarok24135@gmail.com>
2026-05-21 19:09:53 +0400

    Update LiquidGlass.kt

--- app/src/main/java/com/velora/app/ui/components/LiquidGlass.kt
+++ app/src/main/java/com/velora/app/ui/components/LiquidGlass.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.BlurMaskFilter
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.*
